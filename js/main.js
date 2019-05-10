8679da782a196c06a3eef23e838fb0b2187b8158
bassiebangura <devbassiebangura@gmail.com>
2019-05-10 14:35:32 -0400

    Working version of modal.

--- js/main.js
+++ js/main.js
@@ -20,10 +20,10 @@ navItem.addEventListener("click", () => {
 
 /**************************************************
               MODAL POPUP WINDOW
- *************************************************/
-   // Get the modal
-   const modal = document.getElementById('myModal');
-   const modalContentContainer = document.querySelector(".modal-content-container");
+*************************************************/
+// Get the modal
+const modal = document.getElementById('myModal');
+const modalContentContainer = document.querySelector(".modal-content-container");
 
 // Get the element that opens the modal
 let projectCards = document.querySelectorAll(".projects__card");
@@ -32,19 +32,42 @@ let projectCards = document.querySelectorAll(".projects__card");
 let closeSpan = document.querySelector(".close");
 
 // When the user clicks on any card, a modal with that card opens
-projectsCards.forEach((projectCard) => { 
+projectCards.forEach((projectCard) => { 
 
   projectCard.addEventListener('click', (e) => {
       modal.style.display = "block";
-      let listenerTarget = e.currentTarget
-      let employeeCardElementId = listenerTarget.id;
-      //remove elements
-      document.querySelectorAll(".addedElement").forEach(e => {
-          e.parentNode.removeChild(e)
-      })
+      let listenerTarget = e.currentTarget.innerHTML;
+      console.log(e.currentTarget.innerHTML);
+
+      //create Article element
+      let articleAddedToModal = document.createElement("article");
+      articleAddedToModal.setAttribute("class", "addedItem")
+      articleAddedToModal.innerHTML = listenerTarget;
+
+      //remove any prior elements in modal
+      document.querySelectorAll(".addedItem").forEach(e => {
+        e.parentNode.removeChild(e)
+    })
+      console.log(articleAddedToModal)
+      modalContentContainer.appendChild(articleAddedToModal)
+      
+      
   }
   )
 })
+
+ // When the user clicks on <span> (x), close the modal
+let modalCloser = document.querySelector(".close");
+modalCloser.addEventListener('click', () => {
+          modal.style.display = "none";
+          }) 
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function(event) {
+     if (event.target == modal) {
+         modal.style.display = "none";
+     }
+ }
           
 //console.log(employeeCardElementId);
 //console.log(e.currentTarget)
