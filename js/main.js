d138e8ebd57c4fce4285885476f49cc5ce526f77
bassiebangura <devbassiebangura@gmail.com>
2019-05-16 21:57:36 -0400

    Working version.

--- js/main.js
+++ js/main.js
@@ -19,6 +19,89 @@ hamburgerIcon.addEventListener("click", () => {
     return false;
 })
 
+/***********************************************************
+ *        NAV HIGHLIGHT ON SCROLL
+ ************************************************************/
+let mainNavLinks = document.querySelectorAll("nav a");
+let mainSections = document.querySelectorAll("main section");
+
+let lastId;
+let cur = [];
+
+// This should probably be throttled.
+// Especially because it triggers during smooth scrolling.
+// https://lodash.com/docs/4.17.10#throttle
+// You could do like...
+// window.addEventListener("scroll", () => {
+//    _.throttle(doThatStuff, 100);
+// });
+// Only not doing it here to keep this Pen dependency-free.
+
+window.addEventListener("scroll", event => {
+  let fromTop = window.scrollY;
+  console.log("the page has been scrolled")
+  console.log(mainNavLinks);
+  mainNavLinks.forEach(link => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("active");
+    } else {
+      link.classList.remove("active");
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /**************************************************
               MODAL POPUP WINDOW
 *************************************************
